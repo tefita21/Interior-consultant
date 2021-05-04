42cfa52a6dd59d8639ae711f51ef7ed1813932ac
Tefi <estefaniamachado21@gmail.com>
2021-05-04 03:22:43 -0300

    first version

---
+++ javascript.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu").addEventListener("click", () =>{
+    document.querySelector(".nav-bar-container").classList.toggle("show");
+    document.querySelector(".close").classList.toggle("show");
+    });
+
+document.querySelector(".close").addEventListener("click", () =>{
+    document.querySelector(".nav-bar-container").classList.toggle("show");
+    document.querySelector(".close").classList.toggle("show");
+    });
